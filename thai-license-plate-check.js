b6d747c8398f5ca6c7c803b433d4526c8247595d
Wannaphong Phatthiyaphaibun <wannaphong@yahoo.com>
2020-10-22 11:57:16 +0700

    Update thai-license-plate-check.js

--- thai-license-plate-check.js
+++ thai-license-plate-check.js
@@ -2,9 +2,14 @@ function countWords(str) {
     var matches = str.match(/[ก-ฮ]/g);
     return matches ? matches.length : 0;
 }
+function countNum(str) {
+    var matches = str.match(/\d/);
+    return matches ? matches.length : 0;
+}
 function plate_check(p){
-		var i = countWords(p);
-    if((i>2||i==0||p.length>7||/\s/.test(p))&&!p.includes("-")){
+	var i = countWords(p);
+	var i2 = countNum(p);
+    if((i>2||i==0||p.length>7||/\s/.test(p)||i2>4||i2==0)){
     return false;
     }
     return true;
